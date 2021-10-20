2bb415e1f29c64fa9aad70b3175a0d5cb346340c
tuutti <tuutti@iki.fi>
2021-10-20 09:24:40 +0300

    Allow title to be changed and translated

--- tests/src/Functional/FrontControllerTest.php
+++ tests/src/Functional/FrontControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\helfi\Functional;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Url;
 use Drupal\Tests\helfi_api_base\Functional\BrowserTestBase;
 
@@ -18,6 +19,7 @@ class FrontControllerTest extends BrowserTestBase {
    * {@inheritdoc}
    */
   protected static $modules = [
+    'config_translation',
     'helfi_proxy',
   ];
 
@@ -27,6 +29,22 @@ class FrontControllerTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
+   * Asserts page title for given languages.
+   *
+   * @param array $expected
+   *   The expected titles.
+   */
+  private function assertPageTitle(array $expected) : void {
+    foreach (['fi', 'sv', 'en'] as $language) {
+      $this->drupalGet(Url::fromRoute('helfi_proxy.front'), [
+        'query' => ['language' => $language],
+      ]);
+      $this->assertSession()->statusCodeEquals(200);
+      $this->assertSession()->elementTextEquals('css', 'h1', $expected[$language]);
+    }
+  }
+
+  /**
    * Tests the front page.
    */
   public function testFront() : void {
@@ -34,8 +52,27 @@ class FrontControllerTest extends BrowserTestBase {
     $this->assertSession()->statusCodeEquals(200);
 
     $this->drupalLogin($this->rootUser);
-    $this->drupalGet(Url::fromRoute('helfi_proxy.front'));
-    $this->assertSession()->statusCodeEquals(200);
+    $this->assertPageTitle([
+      'fi' => 'Front',
+      'sv' => 'Front',
+      'en' => 'Front',
+    ]);
+
+    /** @var \Drupal\language\ConfigurableLanguageManagerInterface $languageManager */
+    $languageManager = $this->container->get('language_manager');
+
+    foreach (['fi', 'en', 'sv'] as $langcode) {
+      $config = $languageManager->getLanguageConfigOverride($langcode, 'helfi_proxy.settings');
+      $config->set('front_page_title', "Front $langcode")
+        ->save();
+    }
+
+    $this->assertPageTitle([
+      'fi' => 'Front fi',
+      'sv' => 'Front sv',
+      'en' => 'Front en',
+    ]);
+
   }
 
 }
