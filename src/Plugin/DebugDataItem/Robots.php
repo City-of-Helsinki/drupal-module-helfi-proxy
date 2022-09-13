d8c707504363fd792b1be8d73dc6fc3ee0e04e0d
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-09-13 10:12:07 +0300

    UHF-5938: get rid of magic

--- src/Plugin/DebugDataItem/Robots.php
+++ src/Plugin/DebugDataItem/Robots.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 
 namespace Drupal\helfi_proxy\Plugin\DebugDataItem;
 
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\helfi_api_base\DebugDataItemPluginBase;
+use Psr\Container\ContainerInterface;
 
 /**
  * Plugin implementation of the debug_data_item.
@@ -15,13 +17,33 @@ use Drupal\helfi_api_base\DebugDataItemPluginBase;
  *   description = @Translation("Robots data")
  * )
  */
-class Robots extends DebugDataItemPluginBase {
+class Robots extends DebugDataItemPluginBase implements ContainerFactoryPluginInterface {
+
+  protected bool $robots_header;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+
+    try {
+      $instance->robots_header = (bool) $container->get('config.factory')
+        ->get('helfi_proxy.settings')
+        ->get('robots_header_enabled');
+    }
+    catch (\Exception $e) {
+      $instance->robots_header = FALSE;
+    }
+
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function collect(): array {
-    $data['DRUPAL_X_ROBOTS_TAG_HEADER'] = (bool) getenv('DRUPAL_X_ROBOTS_TAG_HEADER');
+    $data['DRUPAL_X_ROBOTS_TAG_HEADER'] = $this->robots_header;
     return $data;
   }
 
