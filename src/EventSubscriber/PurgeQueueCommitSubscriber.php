b74396a8d5b94442df97cc8fe73d3609f6e0c4e3
Marc Galang <marc.galang@digitalist.com>
2025-03-10 15:57:16 +0200

    UHF-11068: Typehint the service directly and remove method_exist check

--- src/EventSubscriber/PurgeQueueCommitSubscriber.php
+++ src/EventSubscriber/PurgeQueueCommitSubscriber.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Drupal\helfi_proxy\EventSubscriber;
 
 use Drupal\helfi_api_base\Event\CacheTagInvalidateEvent;
-use Drupal\purge\Plugin\Purge\Queue\QueueServiceInterface;
+use Drupal\purge\Plugin\Purge\Queue\QueueService;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -18,18 +18,13 @@ final class PurgeQueueCommitSubscriber implements EventSubscriberInterface {
    * Constructs a PurgeQueueCommitSubscriber object.
    */
   public function __construct(
-    #[Autowire('@purge.queue')] private readonly QueueServiceInterface $purgeQueue,
+    #[Autowire('@purge.queue')] private readonly QueueService $purgeQueue,
   ) {}
 
   /**
    * Handles the purge commit event.
    */
   public function onPurgeQueueCommit(): void {
-    if (!method_exists($this->purgeQueue, 'commit')) {
-      // The commit method is mostly used in tests. Check if the method exists
-      // before invoking it.
-      throw new \LogicException('QueueServiceInterface::commit() does not exist anymore.');
-    }
     $this->purgeQueue->commit();
   }
 
