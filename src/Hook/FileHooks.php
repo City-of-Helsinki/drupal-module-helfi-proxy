a4a58fbb41633748a7435a45ed98ea5ffdd9778c
tuutti <tuutti@iki.fi>
2026-09-02 11:01:16 +0300

    UHF-12645: phpstan fixes

--- src/Hook/FileHooks.php
+++ src/Hook/FileHooks.php
@@ -31,7 +31,7 @@ class FileHooks {
    * @see #UHF-7946
    */
   #[Hook('file_url_alter', order: Order::First)]
-  public function fileUrlAlter(&$uri): void {
+  public function fileUrlAlter(string &$uri): void {
     $uri = $this->proxyManager->processPath($uri);
   }
 
